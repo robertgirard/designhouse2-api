24ec58683a02a74bdb9e2a851de8cec1cbe95420
Robert Girard <b.girard@valtechcorp.com>
2021-04-15 18:06:59 -0400

    Combined UploadImageJob into UploadController

--- app/Http/Controllers/Designs/UploadController.php
+++ app/Http/Controllers/Designs/UploadController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Designs;
 
+use Image;
+use File;
 use App\Jobs\UploadImage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
@@ -34,9 +37,55 @@ class UploadController extends Controller
         'disk'  => config('site.upload_disk')
     ]);
 
-
     // dispatch a job to handle the image manipulation
-    $this->dispatch(new UploadImage($design));
+    // $this->dispatch(new UploadImage($design));
+
+    $disk = $design->disk;
+    $filename = $design->image;
+    $original_file = storage_path() . '/uploads/original/'. $filename;
+
+    try{
+        // create the Large Image and save to tmp disk
+        Image::make($original_file)
+            ->fit(800, 600, function($constraint){
+                $constraint->aspectRatio();
+            })
+            ->save($large = storage_path('uploads/large/'. $filename));
+
+        // Create the thumbnail image
+        Image::make($original_file)
+            ->fit(250, 200, function($constraint){
+                $constraint->aspectRatio();
+            })
+            ->save($thumbnail = storage_path('uploads/thumbnail/'. $filename));
+
+        // store images to permanent disk
+        // original image
+        if(Storage::disk($disk)
+            ->put('uploads/designs/original/'.$filename, fopen($original_file, 'r+'))){
+                File::delete($original_file);
+            }
+
+        // large images
+        if(Storage::disk($disk)
+            ->put('uploads/designs/large/'.$filename, fopen($large, 'r+'))){
+                File::delete($large);
+            }
+
+        // thumbnail images
+        if(Storage::disk($disk)
+            ->put('uploads/designs/thumbnail/'.$filename, fopen($thumbnail, 'r+'))){
+                File::delete($thumbnail);
+            }
+
+        // Update the database record with success flag
+        $this->design->update([
+            'upload_successful' => true
+        ]);
+
+    } catch(\Exception $e){
+        \Log::error($e->getMessage());
+    }
 
     return response()->json($design, 200);
 
